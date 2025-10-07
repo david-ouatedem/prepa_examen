5df056c1965124013c27427b3830d8c91cdde3b6
david-ouatedem <davdev1400@gmail.com>
2025-10-07 22:18:05 +0100

    updated exams views

--- app/controllers/admin/exams_controller.rb
+++ app/controllers/admin/exams_controller.rb
@@ -1,14 +1,10 @@
 module Admin
   class ExamsController < Admin::BaseController
-    before_action :set_exam, only: %i[ show edit update destroy ]
+    before_action :set_exam, only: %i[ edit update destroy ]
 
     # GET /exams or /exams.json
     def index
       @exams = Exam.all
-    end
-
-    # GET /exams/1 or /exams/1.json
-    def show
     end
 
     # GET /exams/new
@@ -26,11 +22,14 @@ module Admin
 
       respond_to do |format|
         if @exam.save
-          format.html { redirect_to admin_exam_path(@exam), notice: "Exam was successfully created." }
-          format.json { render :show, status: :created, location: admin_exam_path(@exam) }
+          format.html { redirect_to admin_exams_path, notice: "Exam was successfully created." }
+          format.json { render json: @exam, status: :created }
         else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @exam.errors, status: :unprocessable_entity }
+          format.html do
+            @exams = Exam.all
+            render :index, status: :unprocessable_entity
+          end
+          format.json { render json: { errors: @exam.errors }, status: :unprocessable_entity }
         end
       end
     end
@@ -39,11 +38,14 @@ module Admin
     def update
       respond_to do |format|
         if @exam.update(exam_params)
-          format.html { redirect_to admin_exam_path(@exam), notice: "Exam was successfully updated.", status: :see_other }
-          format.json { render :show, status: :ok, location: admin_exam_path(@exam) }
+          format.html { redirect_to admin_exams_path, notice: "Exam was successfully updated.", status: :see_other }
+          format.json { render json: @exam, status: :ok }
         else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @exam.errors, status: :unprocessable_entity }
+          format.html do
+            @exams = Exam.all
+            render :index, status: :unprocessable_entity
+          end
+          format.json { render json: { errors: @exam.errors }, status: :unprocessable_entity }
         end
       end
     end
@@ -59,14 +61,15 @@ module Admin
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_exam
-        @exam = Exam.find(params[:id])
-      end
 
-      # Only allow a list of trusted parameters through.
-      def exam_params
-        params.require(:exam).permit(:label, :description)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_exam
+      @exam = Exam.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def exam_params
+      params.require(:exam).permit(:label, :description)
+    end
   end
 end
